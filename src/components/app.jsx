26b4a55a9540dd44c6632227d15fd3ac26acd551
ElvisLee <elvislee0725@gmail.com>
2020-04-19 16:10:50 -0700

    Made carousel and about sections

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Menu from './menu';
+import Carousel from './carousel';
+import About from './about';
+
+function App() {
+    return (
+        <div>
+            <Menu />
+            <Carousel />
+            <About />
+        </div>
+    );
+}
+
+export default App;
