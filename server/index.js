6daf66c0a68f1071cd161bb57247efcc00c9800d
ElvisLee <elvislee0725@gmail.com>
2020-07-10 17:03:34 -0700

    Added static-middleware with publicPath to /public  for express to use

--- server/index.js
+++ server/index.js
@@ -3,32 +3,36 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 const sgMail = require('@sendgrid/mail');
+const staticMiddleware = require('./static-middleware');
 sgMail.setApiKey(process.env.SENDGRIDAPIKEY);
+
+app.use(staticMiddleware);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.post('/api/formSend', (req, res) => {
-    const { email, message } = req.body;
-    const msg = {
-        to: 'elvislee0725@gmail.com',
-        from: 'elvis0725@hotmail.com',
-        subject: 'You have a new message from elvislee.com',
-        text: `From: ${email} 
+  const { email, message } = req.body;
+  const msg = {
+    to: 'elvislee0725@gmail.com',
+    from: 'elvis0725@hotmail.com',
+    subject: 'You have a new message from elvislee.com',
+    text: `From: ${email} 
         ${message}`,
-        html: `<p>From: ${email}</p> 
-        <p>${message}</p>`
-    }
+    html: `<p>From: ${email}</p> 
+        <p>${message}</p>`,
+  };
 
-    sgMail.send(msg)
-        .then(() => {
-            console.log(`Message from ${email} was sent successfully.`);
-        })
-        .catch((err) => {
-            console.log('Error: ' + err.message);
-        });
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log(`Message from ${email} was sent successfully.`);
+    })
+    .catch((err) => {
+      console.log('Error: ' + err.message);
+    });
 });
 
 app.listen(process.env.PORT, () => {
-    console.log('Listening on PORT: ', process.env.PORT);
+  console.log('Listening on PORT: ', process.env.PORT);
 });
