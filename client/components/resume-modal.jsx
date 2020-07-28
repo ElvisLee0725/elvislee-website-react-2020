f122149cfc523f6d6fceb22120ec89bfac377187
ElvisLee <elvislee0725@gmail.com>
2020-07-27 23:09:29 -0700

    Update resume sections

--- client/components/resume-modal.jsx
+++ client/components/resume-modal.jsx
@@ -6,82 +6,73 @@ function ResumeModal() {
       <div className='container resume-modal__container'>
         <div className='row'>
           <div className='col-12 col-sm-4'>
-            <h3>ELVIS LEE</h3>
-            <hr />
-            <p>
-              <i className='fa fa-map-marker'></i>&nbsp;Orange County, CA
-              <br />
-              <i className='fa fa-phone'></i>&nbsp;+1 (213) 880-5255 <br />
-              <i className='fa fa-envelope-o'></i>&nbsp;elvislee0725@gmail.com{' '}
-              <br />
-            </p>
-            <hr />
-          </div>
-          <div className='col-12 col-sm-8'>
-            <section className='mb-5'>
-              <h5>EDUCATION</h5>
-              <div>
-                <p>
-                  <b>UNIVERSITY of SOUTHERN CALIFORNIA</b>{' '}
-                  <span className='float-right'>Los Angeles, CA, USA</span>
-                  <br />
-                  Master of Science in Computer Science{' '}
-                  <i>
-                    <span className='float-right'>January 2015</span>
-                  </i>
-                </p>
-              </div>
-              <div>
-                <p>
-                  <b>NATIONAL TSING HUA UNIVERSITY</b>
-                  <span className='float-right'>Hsinchu, Taiwan</span>
-                  <br />
-                  Continuing Education in Computer Science{' '}
-                  <i>
-                    <span className='float-right'>June 2012</span>
-                  </i>
-                </p>
-              </div>
+            <section className='mb-4'>
+              <h3>ELVIS LEE</h3>
+              <hr />
+              <p>
+                <i className='fa fa-map-marker fa-fw'></i>&nbsp;Orange County,
+                CA
+                <br />
+                <i className='fa fa-phone fa-fw'></i>&nbsp;+1 (213) 880-5255
+                <br />
+                <i className='fa fa-envelope-o fa-fw'></i>
+                &nbsp;elvislee0725@gmail.com
+                <br />
+              </p>
+              <hr />
             </section>
 
             <section className='mb-5'>
-              <h5>SKILLS</h5>
+              <h5>TECHNICAL SKILLS</h5>
               <div>
                 <p>
                   <b>Programming Languages</b> <br />
-                  JavaScript, JAVA, PHP, Python, C/C++
+                  JAVA, JavaScript (ES5/ES6), PHP, Python, C/C++, Ruby
                 </p>
               </div>
               <div>
                 <p>
-                  <b>Web Frameworks</b> <br />
-                  Node.js, React, Express, Webpack
-                </p>
-              </div>
-              <div>
-                <p>
-                  <b>Web Development</b> <br />
-                  HTML5, CSS3, Bootstrap, jQuery, JSON, AJAX
-                </p>
-              </div>
-              <div>
-                <p>
-                  <b>Database</b> <br />
+                  <b>Libraries / Frameworks</b> <br />
+                  React.js, Redux, Node.js, Express, PM2, Webpack, NPM, jQuery,
+                  HTML5, CSS3, Bootstrap 4, SASS/SCSS, RESTful APIs, JSON,
                   MySQL, PostgreSQL, MongoDB, Firebase
                 </p>
               </div>
               <div>
                 <p>
-                  <b>Other Tools</b> <br />
-                  GitHub, Bitbucket, AWS, WordPress
+                  <b>Tools</b> <br />
+                  AWS, Nginx, WordPress, GitHub, VS Code, IntelliJ IDEA,
+                  Postman, Meistertask, Figma, DB Designer
                 </p>
               </div>
             </section>
+
             <section className='mb-5'>
-              <h5>EXPERIENCE</h5>
+              <h5>EDUCATION</h5>
               <div>
                 <p>
-                  <b>PalPilot International Corp.</b>{' '}
+                  <b>University of Southern California</b>
+                  <span className='float-right'>Los Angeles, CA, USA</span>
+                  <br />
+                  Master of Science in Computer Science
+                </p>
+              </div>
+              <div>
+                <p>
+                  <b>LearningFuze</b>
+                  <span className='float-right'>Irvine, CA, USA</span>
+                  <br />
+                  Accelerated Web Development Program
+                </p>
+              </div>
+            </section>
+          </div>
+          <div className='col-12 col-sm-8'>
+            <section className='mb-5'>
+              <h5>PROFESSIONAL EXPERIENCE</h5>
+              <div>
+                <p>
+                  <b>PalPilot International Corp.</b>
                   <span className='float-right'>Tustin, CA, USA</span>
                   <br />
                   Web Developer / IT
@@ -91,203 +82,106 @@ function ResumeModal() {
                 </p>
                 <ul>
                   <li>
-                    Assisted design and implemented company website. Maintaining
-                    and updating the contents after its finished.
+                    Redesign and construct a WordPress website for PalPilot
+                    International Corp. to fulfill business requirements while
+                    maintaining and updating the contents of the current WIX
+                    version company website
                   </li>
                   <li>
-                    Integrated SendGrid email API to the company website to get
-                    email notification when visitors showing interests to get in
-                    touch.
+                    Develop a front-end post managing function in WordPress
+                    allowing users to create, update and delete a post without
+                    logging into the WordPress CMS through RESTful APIs and Ajax
                   </li>
                   <li>
-                    Implemented front-end animations and user interactions such
-                    as product carousel, cookies setup and check for improving
-                    user experience, and real-time search using customized CSS3,
-                    JavaScript, jQuery, and PHP.
+                    Refer the WordPress Codex to program customized post types
+                    such as events and products with PHP
                   </li>
                   <li>
-                    Implemented a managing blog posts function on the website
-                    front-end. Users can do Create, Update and Delete without
-                    going to the back-end by RESTful APIs and Ajax. Verifying
-                    logged in users with Nonce.
+                    Implement sharing posts to Facebook with the Facebook API
+                    and Web Dialog tools
                   </li>
                   <li>
-                    Generated customized post types such as events and products
-                    with PHP and referring WordPress Codex.
+                    Apply SendGrid API Libraries to send emails when user leaves
+                    contact, notifying a new lead to follow up
                   </li>
                   <li>
-                    Installed, modified, and created WordPress plugins for the
-                    website to implement customized functions such as image
-                    lightbox and product portfolio.
+                    Design a Unit Converter App to calculate units like length,
+                    area, volume, weight, pressure and temperature according to
+                    the user's inputs programmed in JAVA and apply JavaFX for
+                    the UI and run on Windows OS
                   </li>
                   <li>
-                    Implemented sharing website posts to Facebook function with
-                    the Facebook API and Web Dialog tools.
-                  </li>
-                  <li>
-                    Applied media queries and Bootstrap grid system to reach the
-                    RWD on all kinds of web browsing devices.
-                  </li>
-                  <li>
-                    Created a Converter App to calculate all related units in
-                    length, area, volume, weight, pressure or temp when user
-                    makes an input. Programmed in JAVA, applied JavaFX for UI
-                    and run on Windows OS.
-                  </li>
-                  <li>
-                    Analyzed the HTTP requests made while website connecting and
-                    optimized the loading speed of the website to reduce the
-                    waiting time and improve the fully loaded time by 40%,
-                    taking around 3 seconds.
-                  </li>
-                  <li>
-                    Solving website loading speed slow issue in China by
-                    migrating site to a new host and applying CDN.
-                  </li>
-                  <li>
-                    Implemented SEO with related keywords for each page and
-                    sufficient contents, uploaded XML Sitemap to Google Search
-                    Console, added external links from major social media sites
-                    to the company website.
-                  </li>
-                  <li>
-                    Complied with EU GDPR and other Internet user privacy
-                    policies by updating cookies policy, privacy policy and
-                    terms of use. Installed SSL providing visitors a more secure
-                    connection when submitting info.
-                  </li>
-                  <li>
-                    Assisted the IT department for managing employee accounts,
-                    Intranet domain, email, and FTP. Installed and troubleshot
-                    hardware and software within the office. Maintained web host
-                    and database backups.
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <p>
-                  <b>Information Science Institute</b>{' '}
-                  <span className='float-right'>Los Angeles, CA, USA</span>
-                  <br />
-                  Android Development Intern
-                  <i>
-                    <span className='float-right'>
-                      January 2015 – July 2015
-                    </span>
-                  </i>
-                </p>
-                <ul>
-                  <li>
-                    Assisted developing an Android app facilitating the share of
-                    news in countries with limited Internet access.
-                  </li>
-                  <li>
-                    Added functions such as downloading only popular news, and
-                    enable news being shared on another device.
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <p>
-                  <b>My Eye Media</b>{' '}
-                  <span className='float-right'>Los Angeles, CA, USA</span>
-                  <br />
-                  Web Developer Summer Intern
-                  <i>
-                    <span className='float-right'>June 2014 – August 2014</span>
-                  </i>
-                </p>
-                <ul>
-                  <li>
-                    Built a PHP and JavaScript based online quiz for Quality
-                    Control department interns. User profile and answers are
-                    saved in MySQL database on AWS EC-2. It saves manpower from
-                    manually checking answers paper by paper and speeds up the
-                    recruitment process.
-                  </li>
-                  <li>
-                    Built a website for registered users to log in to the
-                    database and search for information through keywords or a
-                    range of time period. It facilitates company staff and
-                    clients to retrieve data and monitor jobs done online.
+                    Assist the IT to manage employee accounts, email, and FTP to
+                    maintain web hosting and database backups
                   </li>
                 </ul>
               </div>
             </section>
             <section>
-              <h5>PROJECTS</h5>
+              <h5>APPLICATIONS DEVELOPED</h5>
               <div>
                 <p>
-                  <b>Personal Website Re-modal:</b> Updated my site with
-                  Node.js, React for front-end, Express for back-end and Git for
-                  version control.
+                  <b>Super Coupon Pocket</b> <br />A web app that users can keep
+                  track of all coupons they have received easily so they will
+                  never miss a deal
                 </p>
                 <ul>
                   <li>
-                    Applied Node.js as the web development tool to implement
-                    automation and organization of my website. Utilized NPM to
-                    facilitate web development packages installation and version
-                    control.
+                    Implemented a back-end server with Node.js and Express to
+                    define RESTful API endpoints waiting for requests from the
+                    user with a PostgreSQL database to store app data
                   </li>
                   <li>
-                    Wrote OOP JavaScript to realize effects such as scroll to
-                    fade-in and modal. Installed Babel to write ES6 JavaScript.
+                    Constructed the front-end of the app with React.js, styled
+                    with SCSS and Bootstrap 4 utilizing Hooks such as useState
+                    and useEffect from React 16.8 to use state without writing
+                    class components
                   </li>
                   <li>
-                    Utilized Webpack to bundle all JS and SCSS files into one
-                    file for JavaScript and CSS in production.{' '}
+                    Applied JSON Web Token to realize user authentication which
+                    is stored in localStorage after login
                   </li>
                   <li>
-                    Implemented a mobile-first website that loads faster on
-                    mobile devices by serving smaller size images. Built website
-                    responsive to different sizes of screens.
+                    Configured Redux for managing app state with dispatched
+                    actions and reducers
                   </li>
                   <li>
-                    Applied CSS naming method B.E.M. and wrote organized CSS
-                    files with SCSS.
-                  </li>{' '}
-                </ul>
-              </div>
-              <div>
-                <p>
-                  <b>Show Me Restaurants:</b> An Android app lists restaurants
-                  nearby the current location of user’s mobile phone.
-                </p>
-                <ul>
-                  <li>
-                    Imported and parsed restaurants information including image,
-                    rating, address and style from Yelp API.
-                  </li>
-                  <li>
-                    Implemented an Android app to provide a list view and a map
-                    view of the restaurant when it’s selected.
-                  </li>
-                  <li>
-                    Built another demo version of this app with a database
-                    (MySQL) hosted on Amazon EC-2. This web service will sort
-                    and provide restaurant list according to the user’s visit
-                    history and favorite.
+                    Implemented client side routing and private routes for
+                    logged in users only with React Router DOM
                   </li>
                 </ul>
               </div>
               <div>
                 <p>
-                  <b>Weather Master Web App:</b> Aimed to provide weather
-                  information for web and Android users.
+                  <b>Flashcard</b> <br />A flashcard app that users can manage
+                  cards, review them one by one, and mark specific cards for
+                  reviewing
                 </p>
                 <ul>
                   <li>
-                    Built a website backend with PHP/Java Servlet to parse
-                    weather information from Yahoo Weather RSS.
+                    Utilized localStorage to store flashcards created by the
+                    user on the user’s web browser
                   </li>
                   <li>
-                    Implemented a front-end demonstrating result from retrieved
-                    JSON object and applied JavaScript to parse the local time
-                    and showing day or night background images accordingly.
+                    Demonstrated the front-end of the app with React.js and
+                    provided user interaction and progress tracking
                   </li>
                   <li>
-                    Designed an Android app version to perform similar tasks and
-                    support Facebook share with its SDK.
+                    Adapted React Context to facilitate the passing of props
+                    within the component tree
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <p>
+                  <b>Fun Searcher</b> <br />
+                  An event searching app for users to make customized search and
+                  show results in a list and markers on the map
+                </p>
+                <ul>
+                  <li>
+                    Developed with JavaScript OOP, jQuery and applied
+                    Ticketmaster API and Google Maps API
                   </li>
                 </ul>
               </div>
